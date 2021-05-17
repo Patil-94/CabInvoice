4a6601c3d1a36739218b799af85184d6ff41d849
Patil-94 <ankitakhairnar10@.com>
2021-05-17 20:18:23 +0530

    [Ankita] Add:Using UserID Add Total Fare,Number Of Rides

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -19,7 +19,10 @@ namespace Test
             //Creating instance of invoice generator 
             InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
             Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
-
+            RideRepository rideRepository = new RideRepository();
+            string userId = "Dhiraj";
+            rideRepository.AddRide(userId, rides);
+            Ride[] rideData = rideRepository.GetRides(userId);
             //Generating Summary for rides
             InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
             InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0,15);
